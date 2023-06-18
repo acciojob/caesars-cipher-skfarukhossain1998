738fa19b69894d040775c4af87053d07c0d79b09
skfarukhossain1998 <127541802+skfarukhossain1998@users.noreply.github.com>
2023-06-18 16:01:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ function rot13(encodedString){
 }
 
 // You can test your code by running the above function and printing it to console by pressing the run button at the top. To run it with input 36, uncomment the following line
-
+() => { cy.window().then(window => { const result = window.rot13("ABCDEFGHIJKLMNOPQRSTUVWXYZ"); expect(result).to.equal("NOPQRSTUVWXYZABCDEFGHIJKLM"); }); }"
  console.log(rot13("SERR YBIR? NPPVBWBO"));
  console.log(rot13("THE QUICK BROWN FOX JUMPS OVER THE LAZY DOG"));
  console.log(rot13("ABCDEFGHIJKLMNOPQRSTUVWXYZ"));
